9c96c9358293f4e5fa94e45d4e925ce2a25335d2
d.faveria <d.faveria@reply.it>
2019-04-10 22:24:56 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,33 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'sitoMatrimonio';
   isLocked = true;
+  @ViewChild('passwordInput') passwordInput;
+
+
+  ngOnInit() {
+    this.isLocked = !Boolean(localStorage.getItem('token'));
+  }
 
   submitPassword(password: string) {
-    if (password === 'a') {
-      alert('password giusta');
+    // if you get here, well done.
+    if (password.toUpperCase() === 'cocitoap2'.toUpperCase()) {
+      this.isLocked = false;
+      localStorage.setItem('token', 'true');
+    } else {
+      alert('password errata, spiacente');
+    }
+  }
+
+  enterPasswordInput(event: KeyboardEvent) {
+    if (event.key === 'Enter') {
+      this.submitPassword(this.passwordInput.nativeElement.value);
     }
   }
 }
